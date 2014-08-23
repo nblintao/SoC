ad14f14108841327fa37a7de4c5c171cb342ff75
Tao LIN <nblintao@126.com>
2014-08-23 13:40:03 +0800

    vgac.v

--- SoC/top.v
+++ SoC/top.v
@@ -23,7 +23,15 @@ module top( input wire clk,
             output wire [3:0] an,
             output wire [7:0] Led,
             input wire [7:0] sw,
-            input wire [4:0] btn);
+            input wire [4:0] btn,
+input wire PS2KeyboardData,
+input wire PS2KeyboardClk,
+output wire [2:0] vgaRed,
+output wire [2:0] vgaGreen,
+output wire [2:0] vgaBlue,
+output wire Hsync,
+output wire Vsync
+            );
 
     assign Led[7:0] = sw[7:0];
 
@@ -42,22 +50,27 @@ module top( input wire clk,
 
     BTN_Anti M1 (clk, clkdiv, btn, , btn_out, );
     clk_div M2 (clk, 0, sw[2], clkdiv, cpu_clk);
-    multiple_cycle_cpu M3 (cpu_clk, 1, m_addr, d_f_mem, d_t_mem, w_d_mem, wr_vram, rd_vram, io_rdn, state);
+    // multiple_cycle_cpu M3 (cpu_clk, 1, m_addr, d_f_mem, d_t_mem, w_d_mem, wr_vram, rd_vram, io_rdn, state);
     seven_seg M4 (clk, disp_num, sw[1:0], clkdiv[18:17], seg, an);
 
-    assign disp_num = num;
+    // assign disp_num = num;
 
-    reg [31:0] num;
-    // always @(posedge btn_out[0]) begin
-    //     num = num + 1;
-    // end
+    // reg [31:0] num;
+    // always @(posedge btn_out[3]) begin num = num + 1000; end
 
-    // always @(posedge btn_out[1]) begin
-    //     num = num + 10; 
-    // end
-    // always @(posedge btn_out[2]) begin num = num + 100; end
-    always @(posedge btn_out[3]) begin num = num + 1000; end
-    // always @(posedge btn_out[4]) begin num = num + 10000; end
+    reg sys_clk = 1;
+    always @(posedge clk) begin
+        sys_clk <= ~sys_clk;
+    end
+
+    wire [7:0] r,g,b;
+    assign vgaRed = r[7:5];
+    assign vgaGreen = g[7:5];
+    assign vgaBlue[2:1] = b[7:6];
+
+    display_scan_codes M0 (sys_clk, 1'b1, PS2KeyboardClk, PS2KeyboardData,r,g,b,Hsync,Vsync,,,,sw,disp_num);
+
+
 
 
 endmodule
